bd3a9e3cbcca218c09e6fa79bd7063ef6b955eb7
Jun Yang <96882955+junyangggggg@users.noreply.github.com>
2023-03-04 17:46:54 +0800

    Add files via upload

---
+++ easy/66_plus_one.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        lst_of_9 = []
+        if digits[-1] == 9:
+            for i in range(1, len(digits) + 1):
+                if digits[-i] == 9:
+                    lst_of_9.append(-i)
+                else:
+                    break
+
+        if len(lst_of_9) == 0:
+            digits[-1] += 1
+        else:
+            for i in lst_of_9:
+                digits[i] = 0
+            if len(lst_of_9) == len(digits):
+                digits.insert(0, 1)
+            else:
+                digits[lst_of_9[-1] - 1] += 1
+        return digits
