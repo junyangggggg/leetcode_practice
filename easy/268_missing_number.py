7a9e21998568cdc7d164799b2a3d46f345853a04
Jun Yang <96882955+junyangggggg@users.noreply.github.com>
2023-03-08 22:19:09 +0800

    Create 268_missing_number.py

---
+++ easy/268_missing_number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        nums.sort()
+        if nums[0] != 0:
+            return 0
+        
+        for i in range(len(nums) - 1):
+            if nums[i+1] - nums[i] != 1:
+                return nums[i] + 1
+        return nums[-1] + 1
